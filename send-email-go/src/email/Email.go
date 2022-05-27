3073d43ce31844fa625f3a6af901c348049fc6d5
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-05-26 23:31:16 -0300

    Email and Mail Sender

---
+++ send-email-go/src/email/Email.go
@@ -0,0 +1,42 @@
+package email
+
+import (
+	"fmt"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+type Email struct {
+	Emails  []string
+	Subject string
+	Body    string
+}
+
+func NewEmail() *Email {
+	return &Email{}
+}
+
+type MailSender struct {
+	From   string
+	Dailer *gomail.Dialer
+}
+
+func NewMailSender() *MailSender {
+	return &MailSender{}
+}
+
+func (mailsender *MailSender) Send(emailChan chan Email) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", mailsender.From)
+	for ec := range emailChan {
+		m.SetHeader("Subject", ec.Subject)
+		m.SetBody("text/html", ec.Body)
+		for _, to := range ec.Emails {
+			m.SetHeader("To", to)
+			if err := mailsender.Dailer.DialAndSend(m); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
--- send-email-go/src/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/mbcordeiro/send-email-go-kafka-connect-ms
-
-go 1.18
